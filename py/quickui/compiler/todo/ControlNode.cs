dcdfbdca9e736963b95a8b5670fa9e71fd1a5aa7
Pavel Repin <prepin@gmail.com>
2010-02-02 20:01:44 -0800

    Copy compliler C# source to be ported to todo/. Copy various resource files into compiler/ package.

---
+++ py/quickui/compiler/todo/ControlNode.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if DEBUG
+using NUnit.Framework;
+#endif
+
+namespace qc
+{
+    /// <summary>
+    /// An instance of a control found in Quick markup.
+    /// </summary>
+    public class ControlNode : Node
+    {
+        public Control Control { get; set; }
+
+        public ControlNode() : base()
+        {
+        }
+
+        public ControlNode(Control control) : this()
+        {
+            this.Control = control;
+        }
+
+        /// <summary>
+        /// Return the JavaScript to declare a control.
+        /// </summary>
+        public override string EmitJavaScript(int indentLevel)
+        {
+            return Template.Format(
+                "{VariableDeclaration}QuickUI.Control.create({ClassName}{ControlConstructorArguments})",
+                new
+                {
+                    VariableDeclaration = EmitVariableDeclaration(),
+                    ClassName = Control.ClassName,
+                    ControlConstructorArguments = EmitControlConstructorArguments(indentLevel)
+                });
+        }
+
+        /// <summary>
+        /// Return the JavaScript to define a control initial properties.
+        /// </summary>
+        private string EmitControlConstructorArguments(int indentLevel)
+        {
+            return (!Control.Properties.Any())
+                ? String.Empty
+                : Template.Format(
+                    ", {\n{ControlProperties}{Tabs}}",
+                    new
+                    {
+                        ControlProperties = EmitControlProperties(indentLevel + 1),
+                        Tabs = Tabs(indentLevel)
+                    });
+        }
+
+        private string EmitControlProperties(int indentLevel)
+        {
+            StringBuilder code = new StringBuilder();
+            int i = 0;
+            int propertyCount = Control.Properties.Keys.Count;
+
+            // If subcontrol has a content property, write that out first.
+            if (Control.Properties.ContainsKey("content"))
+            {
+                i++;
+                bool isLast = (Control.Properties.Keys.Count == 1);
+                code.Append(EmitControlProperty("content", isLast, indentLevel));
+            }
+
+            // Write out remaining properties.
+            foreach (string propertyName in Control.Properties.Keys)
+            {
+                bool isLast = (++i == propertyCount);
+                if (propertyName != "content")
+                {
+                    code.Append(EmitControlProperty(propertyName, isLast, indentLevel));
+                }
+            }
+
+            return code.ToString();
+        }
+
+        private string EmitControlProperty(string propertyName, bool isLast, int indentLevel)
+        {
+            return Template.Format(
+                "{Tabs}\"{PropertyName}\": {PropertyValue}{Comma}\n",
+                new
+                {
+                    Tabs = Tabs(indentLevel),
+                    PropertyName = propertyName,
+                    PropertyValue = Control[propertyName].EmitJavaScript(indentLevel),
+                    Comma = isLast ? String.Empty : ","
+                });
+        }
+
+#if DEBUG
+        [TestFixture]
+        public class Tests
+        {
+            [Test]
+            public void Control()
+            {
+                Control c = new Control()
+                {
+                    ClassName = "Simple",
+                };
+                c.Properties.Add("id", new HtmlNode("foo"));
+                c.Properties.Add("content", new HtmlNode("Hello"));
+                ControlNode node = new ControlNode()
+                {
+                    Control = c,
+                    Id = "foo"
+                };
+                Assert.AreEqual(
+                    "this.foo = QuickUI.Control.create(Simple, {\n" +
+                    "\t\"content\": \"Hello\",\n" +
+                    "\t\"id\": \"foo\"\n" +
+                    "})",
+                    node.EmitJavaScript());
+            }
+        }
+#endif
+    }
+}
---
+++ py/quickui/compiler/todo/NodeCollection.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if DEBUG
+using NUnit.Framework;
+#endif
+
+namespace qc
+{
+    public class NodeCollection : Node, IEnumerable<Node>
+    {
+        private IEnumerable<Node> Items { get; set; }
+
+        public NodeCollection(IEnumerable<Node> items)
+        {
+            this.Items = items;
+        }
+
+        /// <summary>
+        /// Return the JavaScript for the collection as an array.
+        /// </summary>
+        public override string EmitJavaScript(int indentLevel)
+        {
+            return Template.Format(
+                "[\n{Items}{Tabs}]",
+                new
+                {
+                    Items = EmitItems(indentLevel + 1),
+                    Tabs = Tabs(indentLevel)
+                });
+        }
+
+        /// <summary>
+        /// Return just the items in the collection.
+        /// </summary>
+        public string EmitItems(int indentLevel)
+        {
+            // We have to jump through some hoops to ensure the last element of
+            // the collection does *not* get a comma after it. This means we can't
+            // do a simple LINQ query, so we roll the iteration by hand.
+            StringBuilder code = new StringBuilder();
+            int nodeCount = Items.Count();
+            int i = 0;
+            foreach (Node node in Items)
+            {
+                bool isLast = (++i == nodeCount);
+                code.Append(EmitNodeInCollection(node, isLast, indentLevel));
+            }
+
+            return code.ToString();
+        }
+
+        /// <summary>
+        /// Return the JavaScript to generate a single node.
+        /// </summary>
+        private string EmitNodeInCollection(Node node, bool isLast, int indentLevel)
+        {
+            return Template.Format(
+                "{Tabs}{Node}{Comma}\n",
+                new {
+                    Tabs = Tabs(indentLevel),
+                    Node = node.EmitJavaScript(indentLevel),
+                    Comma = isLast ? String.Empty : ","
+                });
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+
+        public IEnumerator<Node> GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+
+#if DEBUG
+        [TestFixture]
+        public class Tests
+        {
+            [Test]
+            public void Collection()
+            {
+                NodeCollection node = new NodeCollection(new Node[] {
+                    new HtmlNode("Hi, "),
+                    new HtmlNode() {
+                        Id = "content",
+                        Html = "<span id=\"content\"/>"
+                    }
+                });
+
+                Assert.AreEqual(
+                    "[\n" +
+                    "\t\"Hi, \",\n" +
+                    "\tthis.content = $(\"<span id=\\\"content\\\"/>\")[0]\n" +
+                    "]",
+                    node.EmitJavaScript());
+            }
+        }
+#endif
+    }
+}
