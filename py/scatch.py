f433b65ae11267aa2576e7de6ec95ec0af2c186b
Pavel Repin <prepin@gmail.com>
2010-02-03 10:44:50 -0800

    Improve test runner.

--- py/scatch.py
+++ py/scatch.py
@@ -1,20 +1,22 @@
-import quickui.compiler.template
+from quickui.compiler import template
 
-def run_tests(module, verbosity=2, skip_doctests=False):
+def run_tests(*modules, **kwargs):
     """
-    Runs all unit and doc tests present in the executing module, and exits
+    Runs all unit and doc tests present in the specified modules, and exits
     with the appropriate exit code.
     """
     import unittest
     import doctest
     import sys
-    suite = unittest.TestLoader().loadTestsFromModule(module)
-    if doctest.DocTestFinder().find(module):
-        if not skip_doctests:
-            suite.addTest(doctest.DocTestSuite(module))
-    else:
-        raise ValueError("Module has no docstrings, which is a bad thing!")
-    result = unittest.TextTestRunner(verbosity=verbosity).run(suite)
-    sys.exit(not result.wasSuccessful())
 
-run_tests(quickui.compiler.template)
+    verbosity = kwargs.get('verbosity', 2)
+    success = True
+    for module in modules:
+        suite = unittest.TestLoader().loadTestsFromModule(module)
+        if doctest.DocTestFinder().find(module):
+            suite.addTest(doctest.DocTestSuite(module))
+        result = unittest.TextTestRunner(verbosity=verbosity).run(suite)
+        success = success and result.wasSuccessful()
+    sys.exit(0 if success else 1)
+
+run_tests(template)
